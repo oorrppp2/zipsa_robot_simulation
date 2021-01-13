950740d4043ce84ec41a1a9c352dc5f659622039
oorrppp2 <oorrppp@kist.re.kr>
2021-01-13 15:01:06 +0900

    Remove points only for the target object

--- convert_2d_to_3d/src/generate_msg.cpp
+++ convert_2d_to_3d/src/generate_msg.cpp
@@ -35,6 +35,7 @@ class ConvertBoundingBoxNode
         {
 			pointcloud_sub_ = nh_.subscribe<sensor_msgs::PointCloud2>("/camera/depth_registered/points", 1, &ConvertBoundingBoxNode::pointcloud_callback, this);
 			boundingboxes_sub_ = nh_.subscribe<darknet_ros_msgs::BoundingBoxes>("/darknet_ros/bounding_boxes", 1, &ConvertBoundingBoxNode::boundingbox_callback, this);
+			target_id_sub_ = nh_.subscribe<std_msgs::String>("/order_target", 1, &ConvertBoundingBoxNode::target_id_callback, this);
 			pub_result_ = nh_.advertise<convert_2d_to_3d::Result>("detected_object", 1);
 			pointcloud_pub = nh_.advertise<sensor_msgs::PointCloud2>("/camera/depth_registered/removed_object_points", 1);
 
@@ -45,7 +46,7 @@ class ConvertBoundingBoxNode
         {
             delete pointcloud_sub_;
             delete boundingboxes_sub_;
-            delete remove_object_sub_;
+            delete target_id_sub_;
         }
 
 	void boundingbox_callback(const darknet_ros_msgs::BoundingBoxesConstPtr& boundingbox) {
@@ -53,6 +54,10 @@ class ConvertBoundingBoxNode
 		for(int i = 0; i < boundingbox->bounding_boxes.size(); i++) {
 		    gBoundingboxes.bounding_boxes.push_back(boundingbox->bounding_boxes[i]);
 		}
+	}
+
+	void target_id_callback(const std_msgs::StringConstPtr& msg) {
+		target_id = msg->data;
 	}
 
 	void pointcloud_callback(const sensor_msgs::PointCloud2ConstPtr& pointcloud)
@@ -78,13 +83,18 @@ class ConvertBoundingBoxNode
 				// std::cout <<"point : " << p << std::endl;
 				// std::cout <<" ==> " << p.x << " , " << p.y << " , " << p.z << std::endl;
 				// std::cout << "ID : " << id << std::endl;
+				// std::cout << "target_id : " << target_id << std::endl;
 
-				for(int x = gBoundingboxes.bounding_boxes[i].xmin - 5; x < gBoundingboxes.bounding_boxes[i].xmax + 5; x++) {
-					for(int y = gBoundingboxes.bounding_boxes[i].ymin - 5; y < gBoundingboxes.bounding_boxes[i].ymax + 5; y++) {
-						pcl_cloud(x, y).x, pcl_cloud(x, y).y = 0;
-						pcl_cloud(x, y).z = -10;
+				if(id == target_id) {
+					std::cout << target_id << " points removed" << std::endl;
+					for(int x = gBoundingboxes.bounding_boxes[i].xmin - 5; x < gBoundingboxes.bounding_boxes[i].xmax + 5; x++) {
+						for(int y = gBoundingboxes.bounding_boxes[i].ymin - 5; y < gBoundingboxes.bounding_boxes[i].ymax + 5; y++) {
+							pcl_cloud(x, y).x, pcl_cloud(x, y).y = 0;
+							pcl_cloud(x, y).z = -10;
+						}
 					}
 				}
+
 				// Publish result
 				convert_2d_to_3d::Result msg;
 				msg.type = "detected_object";
@@ -116,11 +126,11 @@ class ConvertBoundingBoxNode
         tf2_ros::TransformBroadcaster tfb_;
 	ros::Subscriber pointcloud_sub_;
 	ros::Subscriber boundingboxes_sub_;
-	ros::Subscriber remove_object_sub_;
+	ros::Subscriber target_id_sub_;
 	ros::Publisher pub_result_;
 	ros::Publisher pointcloud_pub;
 	darknet_ros_msgs::BoundingBoxes gBoundingboxes;
-	bool remove_object;
+	std::string target_id;
 };
 
 int main(int argc, char** argv)
